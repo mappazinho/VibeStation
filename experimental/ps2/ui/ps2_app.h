cce34ca121700871dafdcbaf05a82e2a79a2ee31
mappazinho <mappazinho@gmail.com>
2026-09-25 13:21:46 +0300

    PS2 UI: expose second-generation dynarec toggle

--- experimental/ps2/ui/ps2_app.h
+++ experimental/ps2/ui/ps2_app.h
@@ -20,6 +20,7 @@ public:
     void shutdown();
     bool launch_bios(const std::string& path);
     void set_ee_jit_enabled(bool enabled);
+    void set_ee_dynarec_enabled(bool enabled);
     void capture_visible_window(
         const std::string& path,
         unsigned long long minimum_ee_instructions = 0);
